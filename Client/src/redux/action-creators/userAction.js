f016ebbf21c90233bd2ed35a60c1cfd4cde58099
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-06-16 17:11:19 +0530

    [Halfway] redux login

---
+++ Client/src/redux/action-creators/userAction.js
@@ -0,0 +1,36 @@
+import {
+  LOGIN_USER,
+  LOGIN_USER_FAILURE,
+  LOGIN_USER_SUCCESS,
+} from '../action-types/';
+
+import { loginUser } from '../../api/user';
+
+export const signInUser = (email, password) => async (dispatch) => {
+  dispatch({ type: LOGIN_USER });
+  try {
+    const { data } = await loginUser(email, password);
+    console.log(data);
+    dispatch({
+      type: LOGIN_USER_SUCCESS,
+      payload: data,
+    });
+
+    //   set userdata in localStorage
+
+    localStorage.setItem('userData', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: LOGIN_USER_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// export const signOutUser = () => async (dispatch) => {
+//   localStorage.removeItem('userData');
+//   dispatch({ type: LOGOUT_USER });
+// };
